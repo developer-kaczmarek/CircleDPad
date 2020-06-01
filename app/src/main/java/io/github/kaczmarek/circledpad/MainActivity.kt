3b4d49cedc7eaa91420c0c61665981cdc7e4a428
Angelina <deleloper-kaczmarek@yandex.ru>
2020-06-01 15:14:00 +0400

    add clickListener and click processing

--- app/src/main/java/io/github/kaczmarek/circledpad/MainActivity.kt
+++ app/src/main/java/io/github/kaczmarek/circledpad/MainActivity.kt
@@ -2,10 +2,30 @@ package io.github.kaczmarek.circledpad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import io.github.kaczmarek.circledpad.CircleDPad.Companion.BOTTOM_BUTTON
+import io.github.kaczmarek.circledpad.CircleDPad.Companion.CENTER_BUTTON
+import io.github.kaczmarek.circledpad.CircleDPad.Companion.LEFT_BUTTON
+import io.github.kaczmarek.circledpad.CircleDPad.Companion.RIGHT_BUTTON
+import io.github.kaczmarek.circledpad.CircleDPad.Companion.TOP_BUTTON
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CircleDPad.OnClickCircleDPadListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cdp.apply {
+            listener = this@MainActivity
+        }
+    }
+
+    override fun onClickButton(button: Int) {
+        when (button) {
+            LEFT_BUTTON -> Toast.makeText(this, "LEFT_BUTTON", Toast.LENGTH_SHORT).show()
+            TOP_BUTTON -> Toast.makeText(this, "TOP_BUTTON", Toast.LENGTH_SHORT).show()
+            RIGHT_BUTTON -> Toast.makeText(this, "RIGHT_BUTTON", Toast.LENGTH_SHORT).show()
+            BOTTOM_BUTTON -> Toast.makeText(this, "BOTTOM_BUTTON", Toast.LENGTH_SHORT).show()
+            CENTER_BUTTON -> Toast.makeText(this, "CENTER_BUTTON", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- circledpad/src/main/java/io/github/kaczmarek/circledpad/CircleDPad.kt
+++ circledpad/src/main/java/io/github/kaczmarek/circledpad/CircleDPad.kt
@@ -1,17 +1,16 @@
 package io.github.kaczmarek.circledpad
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import androidx.annotation.IntDef
 import androidx.core.content.ContextCompat
 import androidx.core.content.withStyledAttributes
-import kotlin.math.abs
-import kotlin.math.cos
-import kotlin.math.min
-import kotlin.math.sin
+import kotlin.math.*
 
 
 class CircleDPad @JvmOverloads constructor(
@@ -19,30 +18,38 @@ class CircleDPad @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyle: Int = 0
 ) : View(context, attrs, defStyle) {
-    var onClickCircleDPadListener: OnClickCircleDPadListener? = null
-    private var dividersWidth = 2f
-    private var dividersColor = ContextCompat.getColor(context, R.color.PrimaryRedEnabled)
-    private var centerButtonPercentage = .3f
-    private var centerButtonRadius = 0f
-    private var centerButtonColor: ColorStateList? = ContextCompat.getColorStateList(context, R.color.center_button_color)
-    private var centerButtonPaint: Paint
-    private var dividersPaint: Paint
-    private val drawableButtonResources = IntArray(4)
-    private val crossButtonsColors = arrayOfNulls<ColorStateList>(4)
+    private val centerButtonPaint: Paint
+    private val dividersPaint: Paint
     private val crossPaint: Paint
     private val pressedState = BooleanArray(5)
+    private var centerButtonRadius = 0f
+
+    private val drawableButtonResources = IntArray(4)
+    private val crossButtonsColors = arrayOfNulls<ColorStateList>(4)
+
+    var listener: OnClickCircleDPadListener? = null
+    
+    private var dividersWidth = 2f
+    private var centerButtonPercentage = .3f
+
+    private var centerButtonColor: ColorStateList? =
+        ContextCompat.getColorStateList(context, R.color.center_button_color)
+
+    private var dividersColor = ContextCompat.getColor(context, R.color.PrimaryRedEnabled)
 
 
     interface OnClickCircleDPadListener {
-        fun onClickButton(section: Int)
+        fun onClickButton(@DPadButton button: Int)
     }
-
 
     init {
 
         context.withStyledAttributes(attrs, R.styleable.CircleDPad) {
             dividersWidth = getDimensionPixelSize(R.styleable.CircleDPad_dividersWidth, 2).toFloat()
-            dividersColor = getColor(R.styleable.CircleDPad_dividersColor, ContextCompat.getColor(context, R.color.PrimaryRedEnabled))
+            dividersColor = getColor(
+                R.styleable.CircleDPad_dividersColor,
+                ContextCompat.getColor(context, R.color.PrimaryRedEnabled)
+            )
             centerButtonPercentage = getFloat(R.styleable.CircleDPad_centerButtonPercentage, .3f)
             drawableButtonResources[LEFT_BUTTON] =
                 getResourceId(
@@ -63,14 +70,19 @@ class CircleDPad @JvmOverloads constructor(
                 )
 
             crossButtonsColors[LEFT_BUTTON] =
-                getColorStateList(R.styleable.CircleDPad_leftButtonColor) ?: ContextCompat.getColorStateList(context, R.color.cross_buttons_color)
+                getColorStateList(R.styleable.CircleDPad_leftButtonColor)
+                    ?: ContextCompat.getColorStateList(context, R.color.cross_buttons_color)
             crossButtonsColors[TOP_BUTTON] =
-                getColorStateList(R.styleable.CircleDPad_topButtonColor) ?: ContextCompat.getColorStateList(context, R.color.cross_buttons_color)
+                getColorStateList(R.styleable.CircleDPad_topButtonColor)
+                    ?: ContextCompat.getColorStateList(context, R.color.cross_buttons_color)
             crossButtonsColors[RIGHT_BUTTON] =
-                getColorStateList(R.styleable.CircleDPad_rightButtonColor) ?: ContextCompat.getColorStateList(context, R.color.cross_buttons_color)
+                getColorStateList(R.styleable.CircleDPad_rightButtonColor)
+                    ?: ContextCompat.getColorStateList(context, R.color.cross_buttons_color)
             crossButtonsColors[BOTTOM_BUTTON] =
-                getColorStateList(R.styleable.CircleDPad_bottomButtonColor) ?: ContextCompat.getColorStateList(context, R.color.cross_buttons_color)
-            centerButtonColor = getColorStateList(R.styleable.CircleDPad_centerButtonColor) ?: ContextCompat.getColorStateList(context, R.color.center_button_color)
+                getColorStateList(R.styleable.CircleDPad_bottomButtonColor)
+                    ?: ContextCompat.getColorStateList(context, R.color.cross_buttons_color)
+            centerButtonColor = getColorStateList(R.styleable.CircleDPad_centerButtonColor)
+                ?: ContextCompat.getColorStateList(context, R.color.center_button_color)
         }
         crossPaint = Paint(Paint.ANTI_ALIAS_FLAG)
         centerButtonPaint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -85,18 +97,15 @@ class CircleDPad @JvmOverloads constructor(
         val halfHeight = height / 2f
         val radius = min(halfHeight, halfWidth)
         centerButtonRadius = radius * min(abs(centerButtonPercentage), 1.0f)
-        val halfInnerRadius = centerButtonRadius / 2
         val background = background
         background?.draw(canvas)
         val rect = Rect()
         getDrawingRect(rect)
-        val rectF = RectF(rect)
-        val angleOffset = 45
         for (i in 0 until pressedState.size - 1) {
-            val startAngle = angleOffset + 90 * i
+            val startAngle = 45 + 90 * i
             drawCrossButton(
                 canvas,
-                rectF,
+                RectF(rect),
                 crossButtonsColors[i]!!,
                 startAngle,
                 pressedState[i]
@@ -104,25 +113,15 @@ class CircleDPad @JvmOverloads constructor(
         }
 
         for (i in 0 until pressedState.size - 1) {
-            val startAngle = angleOffset + 90 * i
+            val startAngle = 45 + 90 * i
             drawDividers(canvas, radius, startAngle, halfWidth, halfHeight)
+            drawImage(canvas, i, halfWidth, halfHeight, centerButtonRadius / 2)
         }
 
-        drawImage(canvas, LEFT_BUTTON, halfWidth, halfHeight, halfInnerRadius)
-        drawImage(canvas, TOP_BUTTON, halfWidth, halfHeight, halfInnerRadius)
-        drawImage(canvas, RIGHT_BUTTON, halfWidth, halfHeight, halfInnerRadius)
-        drawImage(canvas, BOTTOM_BUTTON, halfWidth, halfHeight, halfInnerRadius)
-
-        if (centerButtonRadius > 0) {
-            centerButtonPaint.style = Paint.Style.FILL
-            centerButtonColor?.defaultColor?.let {
-                centerButtonPaint.color = it
+        centerButtonColor?.let {
+            if (centerButtonRadius > 0) {
+                drawCenterButton(canvas, halfWidth, halfHeight, it, pressedState[CENTER_BUTTON])
             }
-            canvas.drawCircle(halfWidth, halfHeight, centerButtonRadius, centerButtonPaint)
-            centerButtonPaint.style = Paint.Style.STROKE
-            centerButtonPaint.strokeWidth = dividersWidth
-            centerButtonPaint.color = dividersColor
-            canvas.drawCircle(halfWidth, halfHeight, centerButtonRadius, centerButtonPaint)
         }
     }
 
@@ -138,6 +137,21 @@ class CircleDPad @JvmOverloads constructor(
             color.defaultColor
         ) else color.defaultColor
         canvas.drawArc(rectF, startAngle.toFloat(), 90f, true, crossPaint)
+    }
+
+    private fun drawCenterButton(
+        canvas: Canvas,
+        halfWidth: Float,
+        halfHeight: Float,
+        color: ColorStateList,
+        isPressed: Boolean
+    ) {
+        centerButtonPaint.style = Paint.Style.FILL
+        centerButtonPaint.color = if (isPressed) color.getColorForState(
+            intArrayOf(android.R.attr.state_pressed),
+            color.defaultColor
+        ) else color.defaultColor
+        canvas.drawCircle(halfWidth, halfHeight, centerButtonRadius, centerButtonPaint)
     }
 
     private fun drawDividers(
@@ -197,15 +211,74 @@ class CircleDPad @JvmOverloads constructor(
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val action = event.action
+        val x = event.x - width / 2f
+        val y = event.y - height / 2f
+        if (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE) {
+            val radius = sqrt(x.pow(2) + y.pow(2))
+            if (radius > width / 2f || radius > height / 2f) {
+                // click outside the drawn view
+                val cancelEvent = MotionEvent.obtain(event)
+                cancelEvent.action = MotionEvent.ACTION_CANCEL
+                dispatchTouchEvent(cancelEvent)
+                cancelEvent.recycle()
+                updatePressedState()
+                return false
+            }
+            if (radius < centerButtonRadius) { // CenterButton click
+                updatePressedState()
+                pressedState[CENTER_BUTTON] = true
+            } else { // CrossButtons click
+                var touchAngle = Math.toDegrees(
+                    atan2(y.toDouble(), x.toDouble())
+                ).toFloat()
+                if (touchAngle < 0) {
+                    touchAngle += 360f
+                }
+                for (i in 0 until pressedState.size - 1) {
+                    val startAngle = 45 + 90 * i % 360
+                    var endAngle = (startAngle + 90) % 360
+                    if (startAngle > endAngle) {
+                        if (touchAngle < startAngle && touchAngle < endAngle) {
+                            touchAngle += 360f
+                        }
+                        endAngle += 360
+                    }
+                    if (startAngle <= touchAngle && endAngle >= touchAngle) {
+                        updatePressedState()
+                        pressedState[i] = true
+                    }
+                }
+            }
+        } else if (event.action == MotionEvent.ACTION_UP) {
+            pressedState.forEachIndexed { index, b ->
+                if (b) {
+                    listener?.onClickButton(index)
+                }
+            }
+            updatePressedState()
+        }
+        this.invalidate()
+        return true
+    }
+
+    private fun updatePressedState() {
+        for (i in pressedState.indices) {
+            pressedState[i] = false
+        }
+    }
+
     companion object {
-        @IntDef(LEFT_BUTTON, TOP_BUTTON, RIGHT_BUTTON, BOTTOM_BUTTON, CENTER_BUTTON)
+        @IntDef(BOTTOM_BUTTON, LEFT_BUTTON, TOP_BUTTON, RIGHT_BUTTON, CENTER_BUTTON)
         @Retention(AnnotationRetention.SOURCE)
         annotation class DPadButton
 
-        const val LEFT_BUTTON = 0
-        const val TOP_BUTTON = 1
-        const val RIGHT_BUTTON = 2
-        const val BOTTOM_BUTTON = 3
+        const val BOTTOM_BUTTON = 0
+        const val LEFT_BUTTON = 1
+        const val TOP_BUTTON = 2
+        const val RIGHT_BUTTON = 3
         const val CENTER_BUTTON = 4
     }
 }
